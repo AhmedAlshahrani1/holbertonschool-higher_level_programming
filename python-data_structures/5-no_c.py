2914314d82b9d1305440ac191da1cfea2c1fbde5
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-06-13 19:21:39 +0300

    Task 5: Can you C me now?

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-     new_string = ""
-     for charecter in my_string:
-         if charecter != "c" and charecter != "C":
-             new_string += charecter 
-     return new_string
-
+    new_string = ""
+    for charecter in my_string:
+        if charecter != "c" and charecter != "C":
+            new_string += charecter
+    return new_string
