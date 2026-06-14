42541f5dda81817ae8ceaac7b9e610ef4589a696
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-06-14 18:57:46 +0300

    Task 9: Find the max

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,6 +4,6 @@ def max_integer(my_list=[]):
         return None
     biggest_num = my_list[0]
     for num in my_list:
-        if num >  biggest_num:
+        if num > biggest_num:
             biggest_num = num
     return (biggest_num)
