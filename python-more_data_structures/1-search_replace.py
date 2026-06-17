cd1589edd6c1b25c8b510621eb68ff9cb81ca7e3
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-06-17 21:27:49 +0300

    Task 1: Search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -6,14 +6,3 @@ def search_replace(my_list, search, replace):
             num = replace
         new_list.append(num)
     return new_list
-
-   
-    """
-    new_list = []
-    for item in my_list:
-        if item == search:
-            new_list.append(replace)
-        else:
-            new_list.append(item)
-    return new_list
-    """
