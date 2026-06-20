87430f8f10ac0559ef6c9bcaac4818df72256bb3
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-06-20 11:55:17 +0300

    Task 3: Area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that defines a square."""
+
+
+class Square:
+    """Class that represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes the square."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size * self.__size
