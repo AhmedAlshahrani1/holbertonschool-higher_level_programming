2cbfd468c703634c7de46603d64c353ae9b044dc
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-06-20 11:41:42 +0300

    Task 2: Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -20,9 +20,10 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
+        
         if size < 0:
             raise ValueError("size must be >= 0")
-
+        
         self.__size = size
